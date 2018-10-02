2622530843b608d72a7231a545f05a530f47224b
Luis Angel Mojena Román <lamojena@estudiantes.uci.cu>
2018-10-02 20:37:23 +0000

    Add a manage.py file for recurrent development tasks

---
+++ manage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from flask_script import Manager
+
+from app import app, db
+
+manager = Manager(app)
+
+
+@manager.command
+def createdb(drop_first=False):
+    """Creates the database"""
+    if drop_first:
+        db.drop_all()
+    db.create_all()
+
+
+if __name__ == '__main__':
+    manager.run()
