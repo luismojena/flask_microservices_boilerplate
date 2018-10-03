f3bbdf0f62854f48d1dbf55823c3348cd1deddd6
Luis Angel Mojena Román <lamojena@estudiantes.uci.cu>
2018-10-03 01:43:44 +0000

    Add primary key example

--- app/models.py
+++ app/models.py
@@ -8,7 +8,8 @@ class ExampleModel(db.Model):
     """
     Example model
     """
-    example_string_field: str = db.Column(db.String(20), nullable=True, unique=False)
+    example_string_field: str = db.Column(db.String(20), nullable=True,
+                                          unique=False, primary_key=True)
 
     def example(self):
         return self.example_string_field
