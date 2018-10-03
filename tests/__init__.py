e934bdb3e7ee65a3af4aaffe5683bdf599fb12a4
Luis Angel Mojena Román <lamojena@estudiantes.uci.cu>
2018-10-03 00:34:54 +0000

    BUGFIX: APP_CONFIG

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,7 +6,7 @@ import coverage
 
 
 def run():
-    os.environ['FLASK_CONFIG'] = 'testing'
+    os.environ['APP_CONFIG'] = 'testing'
 
     # start coverage engine
     coverage_engine = coverage.Coverage(branch=True)
