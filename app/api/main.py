ff80d53a3d577822c2c2ed66aca71b3ddc42b6a2
Luis Angel Mojena Román <lamojena@estudiantes.uci.cu>
2018-10-03 01:46:20 +0000

    Refactor to avoid name collision on blueprint

--- app/api/main.py
+++ app/api/main.py
@@ -5,13 +5,13 @@ Main blueprint for the flask app
 from flask import Blueprint
 from flask import jsonify
 
-main = Blueprint('main', __name__)
+api_blueprint = Blueprint('api', __name__)
 
 
-@main.route('/', methods=['GET'])
+@api_blueprint.route('/', methods=['GET'])
 def index():
     """
     Index route from main blueprint
     :return:
     """
-    return jsonify({'Hello': 'World!!'})
+    return jsonify({'Hello': 'API!!'})
