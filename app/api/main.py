d2b12856c1db76ad2e1b9a2a7b56c5bc800b46cc
Luis Angel Mojena Román <lamojena@estudiantes.uci.cu>
2018-10-03 01:21:34 +0000

    Add module and function docstring

--- app/api/main.py
+++ app/api/main.py
@@ -1,3 +1,7 @@
+"""
+Main blueprint for the flask app
+"""
+
 from flask import Blueprint
 from flask import jsonify
 
@@ -6,4 +10,8 @@ main = Blueprint('main', __name__)
 
 @main.route('/', methods=['GET'])
 def index():
+    """
+    Index route from main blueprint
+    :return:
+    """
     return jsonify({'Hello': 'World!!'})
